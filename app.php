710b4c1682f8735f08318539fe53bba67365994e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 16:47:58 +0200

    Update app.php

--- app.php
+++ app.php
@@ -85,5 +85,4 @@ return [
     'locale' => 'en',
 
     /*
-    |--------------------------------------------------------------------------
     |    | the language folders that are provided through your application.
