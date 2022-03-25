a3695eb3254a7d48ac32dc364612760efbead902
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 15:34:33 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -100,5 +100,4 @@ return [
     'not_in' => 'The selected :attribute is invalid.',
     'not_regex' => 'The :attribute format is invalid.',
     'numeric' => 'The :attribute must be a number.',
-    'password' => 'The password is incorrect.',
     
