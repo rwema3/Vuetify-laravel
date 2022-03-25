701097b2d44edf9e0d3cbcabba5b8ec459b5eea5
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 16:50:11 +0200

    Update app.php

--- app.php
+++ app.php
@@ -50,5 +50,4 @@ return [
     |
     | This URL is used by the console to properly generate URLs when using
     | the Artisan command line tool. You should set this to the root of
-    | your application so that it is used when running Artisan tasks.
     |
