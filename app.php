fdbeded93eab0a06f49e05cfd5ba06a5c642c645
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 16:58:35 +0200

    Update app.php

--- app.php
+++ app.php
@@ -9,4 +9,4 @@ return [
     | Application Name
     |--------------------------------------------------------------------------
     |
-    | This value is the name of your application. Thi
+    | This value is the name of your application. Th
