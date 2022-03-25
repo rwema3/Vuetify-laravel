1c7a7f92f7561a163c94dc6f0004fd7f66f78815
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 16:25:55 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -47,4 +47,4 @@ return [
     'ends_with' => 'The :attribute must end with one of the following: :values.',
     'enum' => 'The selected :attribute is invalid.',
     'exists' => 'The selected :attribute is invalid.',
-    'file' => 'The :attribute must be
+    'file' => 'The :attribute must b
