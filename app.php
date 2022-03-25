4773a4da0f3e1d719c1a60bec50ec61fa54f16b0
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:11:58 +0200

    | application. If disabled, a simple generic error page is shown.

--- app.php
+++ app.php
@@ -37,3 +37,4 @@ return [
     |
     | When your application is in debug mode, detailed error messages with
     | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
