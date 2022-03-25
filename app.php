4372dac636bbe972eca7d598d7966f85a1247bdc
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:11:10 +0200

    | services the application utilizes. Set this in your ".env" file.

--- app.php
+++ app.php
@@ -24,3 +24,5 @@ return [
     |
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
+    | services the application utilizes. Set this in your ".env" file.
+    |
