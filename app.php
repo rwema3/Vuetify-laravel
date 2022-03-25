4714b92065395873cd91e1168b763937d8ab7e92
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:14:54 +0200

    | by the translation service provider. You are free to set this value

--- app.php
+++ app.php
@@ -78,3 +78,4 @@ return [
     |
     | The application locale determines the default locale that will be used
     | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
