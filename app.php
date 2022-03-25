eb36e6a4ee0f7422f1d2d1a37185f6ecb93891cb
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 16:46:23 +0200

    Update app.php

--- app.php
+++ app.php
@@ -116,4 +116,3 @@ return [
     |--------------------------------------------------------------------------
     |
     | This key is used by the Illuminate encrypter service and should be set
-    | to a random, 32 character string, otherwise these encrypted strings
