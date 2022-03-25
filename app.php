eac97d010fc7a4712bc26dc93cc41ed40e5c5735
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:20:24 +0200

    App\Providers\AuthServiceProvider::class,

--- app.php
+++ app.php
@@ -172,3 +172,5 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
+        App\Providers\AuthServiceProvider::class,
+        // App\Providers\BroadcastServiceProvider::class,
