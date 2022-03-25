ce62245b263fa6f86f843c309e1221b9be635a74
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:14:16 +0200

    | ahead and set this to a sensible default for you out of the box.

--- app.php
+++ app.php
@@ -66,3 +66,4 @@ return [
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. We have gone
     | ahead and set this to a sensible default for you out of the box.
+    |
