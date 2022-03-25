19898a19d00db992af005ba6b9ae59bc53a33b7c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 15:34:37 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -99,5 +99,4 @@ return [
     'multiple_of' => 'The :attribute must be a multiple of :value.',
     'not_in' => 'The selected :attribute is invalid.',
     'not_regex' => 'The :attribute format is invalid.',
-    'numeric' => 'The :attribute must be a number.',
     
