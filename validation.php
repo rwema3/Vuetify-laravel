66127e210c6d5e73faab9f9a32fde25fde635d33
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 15:36:55 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -58,5 +58,4 @@ return [
     'gte' => [
         'array' => 'The :attribute must have :value items or more.',
         'file' => 'The :attribute must be greater than or equal to :value kilobytes.',
-        'numeric' => 'The :attribute must be greater than or equal to :value.',
     
