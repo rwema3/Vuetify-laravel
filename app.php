67762e9b82d5058810d3c1184a83d6e6c9409fc9
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:17:31 +0200

    'key' => env('APP_KEY'),

--- app.php
+++ app.php
@@ -119,3 +119,6 @@ return [
     | to a random, 32 character string, otherwise these encrypted strings
     | will not be safe. Please do this before deploying an application!
     |
+    */
+
+    'key' => env('APP_KEY'),
