293ed022537f8d4685a48a3ffea1f7725df146b0
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 15:23:02 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -114,4 +114,3 @@ return [
     'required_with' => 'The :attribute field is required when :values is present.',
     'required_with_all' => 'The :attribute field is required when :values are present.',
     'required_without' => 'The :attribute field is required when :values is not present.',
-    'required_without_all' => 'The :attribute field is required when none of :values are present.',
