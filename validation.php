93e44010e0c928daf4d58fc9a70b0c12e478e93e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 14:49:10 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -153,4 +153,3 @@ return [
     |--------------------------------------------------------------------------
     |
     | The following language lines are used to swap our attribute placeholder
-    | with something more reader friendly such as "E-Mail Address" instead
