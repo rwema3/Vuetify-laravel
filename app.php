64999a3db461f248e05892905cdc852a352585b8
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 16:45:01 +0200

    Update app.php

--- app.php
+++ app.php
@@ -133,4 +133,3 @@ return [
     | The service providers listed here will be automatically loaded on the
     | request to your application. Feel free to add your own services to
     | this array to grant expanded functionality to your applications.
-    |
