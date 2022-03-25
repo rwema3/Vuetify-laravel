32e7e3622ceeb83c2054996e2cf6c7c66925bbb7
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 16:53:24 +0200

    Update app.php

--- app.php
+++ app.php
@@ -10,4 +10,4 @@ return [
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a n
+    | framework needs to place the application's name in a 
