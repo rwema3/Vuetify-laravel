ac96f709ad66cca6cc686ceebc1a5a14a94b879c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:20:31 +0200

    App\Providers\EventServiceProvider::class,

--- app.php
+++ app.php
@@ -174,3 +174,4 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
