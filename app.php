89c10dbf4ff7f8f9fbe0f9dff5122357aa419aa9
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 16:51:05 +0200

    Update app.php

--- app.php
+++ app.php
@@ -35,4 +35,3 @@ return [
     | Application Debug Mode
     |--------------------------------------------------------------------------
     |
-    | When your application is in debug mode, detailed error messages with
