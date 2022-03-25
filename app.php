db5c379dc86008e616b32c387984fc65cc7665fc
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:20:52 +0200

    | Class Aliases

--- app.php
+++ app.php
@@ -177,3 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Aliases
+    |--------------------------------------------------------------------------
