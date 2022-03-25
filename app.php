536d2065a398672fd78e9004d9be2564538b0069
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:21:07 +0200

    | the aliases are "lazy" loaded so they don't hinder performance.

--- app.php
+++ app.php
@@ -187,3 +187,4 @@ return [
     | This array of class aliases will be registered when this application
     | is started. However, feel free to register as many as you wish as
     | the aliases are "lazy" loaded so they don't hinder performance.
+    |
