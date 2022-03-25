ccb6dac6b215d8c7c7f092e08fcbca12996f0146
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 15:21:20 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -138,4 +138,3 @@ return [
     | Here you may specify custom validation messages for attributes using the
     | convention "attribute.rule" to name the lines. This makes it quick to
     | specify a specific custom language line for a given attribute rule.
-    |
