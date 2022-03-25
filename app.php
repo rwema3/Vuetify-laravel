fd3c94350223f7947a1abd1b7422c5d3e43eb059
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:18:28 +0200

    | The service providers listed here will be automatically loaded on the

--- app.php
+++ app.php
@@ -132,3 +132,4 @@ return [
     |
     | The service providers listed here will be automatically loaded on the
     | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
