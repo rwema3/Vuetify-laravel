3c75f0c6dc10926843988f6355ce331f35a74a74
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 15:34:53 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -94,5 +94,4 @@ return [
         'array' => 'The :attribute must have at least :min items.',
         'file' => 'The :attribute must be at least :min kilobytes.',
         'numeric' => 'The :attribute must be at least :min.',
-        'string' => 'The :attribute must be at least :min characters.',
     
