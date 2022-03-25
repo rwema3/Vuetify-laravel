921d158a432c2266b591b651e1a666aaaab3008b
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:15:55 +0200

    |

--- app.php
+++ app.php
@@ -92,3 +92,4 @@ return [
     | The fallback locale determines the locale to use when the current one
     | is not available. You may change the value to correspond to any of
     | the language folders that are provided through your application.
+    |
