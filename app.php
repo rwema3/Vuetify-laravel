e311467a5842b6b6a8a3f584c473229adea30fb0
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:12:48 +0200

    | the Artisan command line tool. You should set this to the root of

--- app.php
+++ app.php
@@ -49,4 +49,5 @@ return [
     |--------------------------------------------------------------------------
     |
     | This URL is used by the console to properly generate URLs when using
+    | the Artisan command line tool. You should set this to the root of
     
