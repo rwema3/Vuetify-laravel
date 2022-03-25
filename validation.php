47373aa5ac533e2d7a1cf36b43344d124736aceb
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 16:19:19 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -50,4 +50,4 @@ return [
     'file' => 'The :attribute must be a file.',
     'filled' => 'The :attribute field must have a value.',
     'gt' => [
-        'array' => 'The :attribute must have more tha
+        'array' => 'The :attribute must have more th
