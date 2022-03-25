573f036e8f566a509bc659d5d48e5ae19db5f61f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:10:15 +0200

    Update app.php

--- app.php
+++ app.php
@@ -12,3 +12,4 @@ return [
     | This value is the name of your application. This value is used when the
     | framework needs to place the application's name in a notification or
     | any other location as required by the application or its packages.
+    |
