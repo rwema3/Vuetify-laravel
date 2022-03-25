b92a8e1e4852b258eab64bc2fb4714d61d8a53eb
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 16:42:36 +0200

    Update app.php

--- app.php
+++ app.php
@@ -162,5 +162,3 @@ return [
         Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
-
