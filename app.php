bcd84e7afcb13c196d3f60ee687f916241c24887
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-25 17:16:33 +0200

    | data for your database seeds. For example, this will be used to get

--- app.php
+++ app.php
@@ -104,4 +104,5 @@ return [
     |
     | This locale will be used by the Faker PHP library when generating fake
     | data for your database seeds. For example, this will be used to get
+    | localized telephone numbers, street address information and more.
     |
